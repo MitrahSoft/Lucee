8f2cbb2ea64b5be2420b7671cd39a781374be27d
Zac Spitzer <zac.spitzer@gmail.com>
2025-12-15 00:40:22 +0100

    LDEV-5991 change default datasource idle timeout to 10m

--- core/src/main/java/lucee/runtime/config/DatasourceConnPool.java
+++ core/src/main/java/lucee/runtime/config/DatasourceConnPool.java
@@ -124,8 +124,9 @@ public final class DatasourceConnPool extends GenericObjectPool<DatasourceConnec
 		config.setMaxIdle(maxIdle > 0 ? maxIdle : GenericObjectPoolConfig.DEFAULT_MAX_IDLE);
 		config.setMaxTotal(maxTotal > 0 ? maxTotal : GenericObjectPoolConfig.DEFAULT_MAX_TOTAL);
 		config.setMaxWaitMillis(maxWaitMillis > 0 ? maxWaitMillis : GenericObjectPoolConfig.DEFAULT_MAX_WAIT_MILLIS);
-		// TDOo merge with idleTimeout
-		config.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis > 0 ? minEvictableIdleTimeMillis : GenericObjectPoolConfig.DEFAULT_MIN_EVICTABLE_IDLE_TIME_MILLIS);
+		// TODO merge with idleTimeout
+		// minEvictableIdleTimeMillis: -1 = infinite (no eviction), >0 = use that value, 0 = use Commons Pool2 default (30 min)
+		config.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis != 0 ? minEvictableIdleTimeMillis : GenericObjectPoolConfig.DEFAULT_MIN_EVICTABLE_IDLE_TIME_MILLIS);
 		// TODO this was done so far by the controler
 		config.setTimeBetweenEvictionRunsMillis(
 				timeBetweenEvictionRunsMillis > 0 ? timeBetweenEvictionRunsMillis : GenericObjectPoolConfig.DEFAULT_TIME_BETWEEN_EVICTION_RUNS_MILLIS);
--- core/src/main/java/lucee/runtime/listener/AppListenerUtil.java
+++ core/src/main/java/lucee/runtime/listener/AppListenerUtil.java
@@ -230,10 +230,10 @@ public final class AppListenerUtil {
 
 			try {
 				int idle = Caster.toIntValue(data.get(IDLE_TIMEOUT, null), -1);
-				if (idle == -1) idle = Caster.toIntValue(data.get(CONNECTION_TIMEOUT, null), 1);
+				if (idle == -1) idle = Caster.toIntValue(data.get(CONNECTION_TIMEOUT, null), -1);
 				return ApplicationDataSource.getInstance(config, name, cd, Caster.toString(oConnStr), user, pass, listener,
 						Caster.toBooleanValue(data.get(KeyConstants._blob, null), false), Caster.toBooleanValue(data.get(KeyConstants._clob, null), false),
-						Caster.toIntValue(data.get(CONNECTION_LIMIT, null), -1), idle, Caster.toIntValue(data.get(LIVE_TIMEOUT, null), 60),
+						Caster.toIntValue(data.get(CONNECTION_LIMIT, null), -1), idle, Caster.toIntValue(data.get(LIVE_TIMEOUT, null), -1),
 						Caster.toIntValue(data.get("minIdle", null), 0), Caster.toIntValue(data.get("maxIdle", null), 0), Caster.toIntValue(data.get("maxTotal", null), 0),
 						Caster.toLongValue(data.get(META_CACHE_TIMEOUT, null), 60000L), timezone, Caster.toIntValue(data.get(ALLOW, null), DataSource.ALLOW_ALL),
 						Caster.toBooleanValue(data.get(KeyConstants._storage, null), false), Caster.toBooleanValue(data.get(KeyConstants._readonly, null), false),
@@ -251,11 +251,11 @@ public final class AppListenerUtil {
 		try {
 
 			int idle = Caster.toIntValue(data.get(IDLE_TIMEOUT, null), -1);
-			if (idle == -1) idle = Caster.toIntValue(data.get(CONNECTION_TIMEOUT, null), 1);
+			if (idle == -1) idle = Caster.toIntValue(data.get(CONNECTION_TIMEOUT, null), -1);
 
 			return new DataSourceImpl(config, name, dbt.classDefinition, Caster.toString(data.get(KeyConstants._host)), dbt.connectionString, Caster.toString(data.get(KeyConstants._BUNDLENAME, null), null), Caster.toString(data.get(KeyConstants._BUNDLEVERSION, null), null),
 					Caster.toString(data.get(KeyConstants._database)), Caster.toIntValue(data.get(KeyConstants._port, null), -1), user, pass, listener,
-					Caster.toIntValue(data.get(CONNECTION_LIMIT, null), -1), idle, Caster.toIntValue(data.get(LIVE_TIMEOUT, null), 1),
+					Caster.toIntValue(data.get(CONNECTION_LIMIT, null), -1), idle, Caster.toIntValue(data.get(LIVE_TIMEOUT, null), -1),
 					Caster.toIntValue(data.get("minIdle", null), 0), Caster.toIntValue(data.get("maxIdle", null), 0), Caster.toIntValue(data.get("maxTotal", null), 0),
 					Caster.toLongValue(data.get(META_CACHE_TIMEOUT, null), 60000L), Caster.toBooleanValue(data.get(KeyConstants._blob, null), false),
 					Caster.toBooleanValue(data.get(KeyConstants._clob, null), false), DataSource.ALLOW_ALL, Caster.toStruct(data.get(KeyConstants._custom, null), null, false),
