a97459b8514241bd77f58913ade2a93b0712c74d
Zac Spitzer <zac.spitzer@gmail.com>
2025-12-30 19:56:59 +0100

    LDEV-6046 sessionInvalidate() de-activate old session when using database storage

--- core/src/main/java/lucee/runtime/type/scope/ScopeContext.java
+++ core/src/main/java/lucee/runtime/type/scope/ScopeContext.java
@@ -504,7 +504,7 @@ public final class ScopeContext {
 		return null;
 	}
 
-	public void removeSessionScope(PageContext pc) throws PageException {
+	public void removeSessionScope(PageContext pc) {
 		removeCFSessionScope(pc);
 		removeJSessionScope(pc);
 	}
@@ -517,24 +517,30 @@ public final class ScopeContext {
 		}
 	}
 
-	public void removeCFSessionScope(PageContext pc) throws PageException {
+	public void removeCFSessionScope(PageContext pc) {
 
 		ApplicationContext appContext = pc.getApplicationContext();
 		Map<String, Scope> context = getSubMap(cfSessionContexts, appContext.getName());
 		if (context != null) {
-			context.remove(pc.getCFID());
-			StorageScope scope = getCFScope(pc, false, Scope.SCOPE_SESSION);
-			if (scope != null) scope.unstore(pc.getConfig());
+			// LDEV-6046: Get scope BEFORE removing from memory, so we can unstore it
+			// Don't use getCFScope() as it would reload from DB and put back in memory
+			Scope scope = context.remove(pc.getCFID());
+			if (scope instanceof StorageScope) {
+				((StorageScope) scope).unstore(pc.getConfig());
+			}
 		}
 	}
 
-	public void removeClientScope(PageContext pc) throws PageException {
+	public void removeClientScope(PageContext pc) {
 		ApplicationContext appContext = pc.getApplicationContext();
 		Map<String, Scope> context = getSubMap(cfClientContexts, appContext.getName());
 		if (context != null) {
-			context.remove(pc.getCFID());
-			StorageScope scope = getCFScope(pc, false, Scope.SCOPE_CLIENT);
-			if (scope != null) scope.unstore(pc.getConfig());
+			// LDEV-6046: Get scope BEFORE removing from memory, so we can unstore it
+			// Don't use getCFScope() as it would reload from DB and put back in memory
+			Scope scope = context.remove(pc.getCFID());
+			if (scope instanceof StorageScope) {
+				((StorageScope) scope).unstore(pc.getConfig());
+			}
 		}
 	}
 
