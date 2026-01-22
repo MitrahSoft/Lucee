771328f2219013b5131dde7396ba033b06fc4060
Michael Offner <michael@lucee.org>
2026-01-22 09:18:56 +0100

    clean up

--- core/src/main/java/lucee/transformer/bytecode/BytecodeContext.java
+++ core/src/main/java/lucee/transformer/bytecode/BytecodeContext.java
@@ -19,7 +19,6 @@
 package lucee.transformer.bytecode;
 
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.Stack;
 import java.util.concurrent.atomic.AtomicLong;
