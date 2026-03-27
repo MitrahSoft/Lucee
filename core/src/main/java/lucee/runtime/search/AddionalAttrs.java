92951d0c266e567fa8464321e82c5cb5979ccd92
Zac Spitzer <zac.spitzer@gmail.com>
2026-03-27 13:38:25 +1100

    LDEV-6196 add contextPassageLength to AddionalAttrs, cleanup Search.java reflection

--- core/src/main/java/lucee/runtime/search/AddionalAttrs.java
+++ core/src/main/java/lucee/runtime/search/AddionalAttrs.java
@@ -24,21 +24,23 @@ public final class AddionalAttrs {
 	private int contextBytes;
 	private String contextHighlightBegin;
 	private int contextPassages;
+	private int contextPassageLength;
 	private String contextHighlightEnd;
 	private int startrow = 1;
 	private int maxrows = -1;
 	private boolean hasRowHandling;
 
-	public AddionalAttrs(int contextBytes, int contextPassages, String contextHighlightBegin, String contextHighlightEnd) {
+	public AddionalAttrs(int contextBytes, int contextPassages, int contextPassageLength, String contextHighlightBegin, String contextHighlightEnd) {
 		this.contextBytes = contextBytes;
 		this.contextPassages = contextPassages;
+		this.contextPassageLength = contextPassageLength;
 		this.contextHighlightBegin = contextHighlightBegin;
 		this.contextHighlightEnd = contextHighlightEnd;
 	}
 
 	public static AddionalAttrs getAddionlAttrs() {
 		AddionalAttrs aa = (AddionalAttrs) addAttrs.get();
-		if (aa == null) aa = new AddionalAttrs(300, 0, "<b>", "</b>");
+		if (aa == null) aa = new AddionalAttrs(300, 0, 150, "<b>", "</b>");
 		return aa;
 	}
 
@@ -46,8 +48,8 @@ public final class AddionalAttrs {
 		addAttrs.set(aa);
 	}
 
-	public static void setAddionalAttrs(int contextBytes, int contextPassages, String contextHighlightBegin, String contextHighlightEnd) {
-		setAddionalAttrs(new AddionalAttrs(contextBytes, contextPassages, contextHighlightBegin, contextHighlightEnd));
+	public static void setAddionalAttrs(int contextBytes, int contextPassages, int contextPassageLength, String contextHighlightBegin, String contextHighlightEnd) {
+		setAddionalAttrs(new AddionalAttrs(contextBytes, contextPassages, contextPassageLength, contextHighlightBegin, contextHighlightEnd));
 	}
 
 	public static void removeAddionalAttrs() {
@@ -73,6 +75,13 @@ public final class AddionalAttrs {
 	 */
 	public int getContextPassages() {
 		return contextPassages;
+	}
+
+	/**
+	 * @return the contextPassageLength
+	 */
+	public int getContextPassageLength() {
+		return contextPassageLength;
 	}
 
 	/**
