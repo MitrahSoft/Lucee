64159cfc172a4a2707d678351f0c19605b261514
Zac Spitzer <zac.spitzer@gmail.com>
2026-05-11 15:40:07 +0200

    LDEV-5893 stop re-enabling deprecated TLS protocols on SMTP STARTTLS path

--- core/src/main/java/lucee/runtime/net/http/sni/SSLConnectionSocketFactoryImpl.java
+++ core/src/main/java/lucee/runtime/net/http/sni/SSLConnectionSocketFactoryImpl.java
@@ -45,4 +45,13 @@ public class SSLConnectionSocketFactoryImpl extends SSLConnectionSocketFactory {
 		return Collections.emptyList();
 	}
 
+	public static List<String> getEnabledSslProtocols() {
+		try {
+			return Arrays.asList(SSLContext.getDefault().getDefaultSSLParameters().getProtocols());
+		}
+		catch (NoSuchAlgorithmException ex) {
+		}
+		return Collections.emptyList();
+	}
+
 }
